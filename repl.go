18cd811880a567ad2b0dc000834216d4168c602d
RShnitser <romanshnitser@hotmail.com>
2024-10-30 19:37:03 -0400

    add scene state

--- repl.go
+++ repl.go
@@ -7,21 +7,52 @@ import(
 	"strings"
 )
 
+type Scene int
+const (
+	Start Scene = iota
+	Travel
+)
+
+type ability struct{
+	name string
+}
+
+type pokemon struct{
+	name string
+	abilities []ability
+}
+
 type gameState struct{
-	running bool	
+	running bool
+	stamina int	
+	scene Scene
 }
 
 func (state *gameState)init(){
 	state.running = true
+	state.stamina = 10
+	state.scene = Start
 }
 
 func (state *gameState)run(){
 	scanner := bufio.NewScanner(os.Stdin)
 	
 	for state.running{
+
+		switch(state.scene){
+		case Start:
+			fmt.Println("Pick your partner:")
+			fmt.Println("[1] Bulbasaur:")
+			fmt.Println("[2] Charmander")
+			fmt.Println("[3] Squirtle")
+		}
 		scanner.Scan()
 		input := scanner.Text()
 		input = strings.ToLower(input)
-		fmt.Println(input)
+
+		if input == "exit"{
+			state.running = false
+		}
+		//fmt.Println(input)
 	}
 }
