4f62ac20e94ace4575c9fe74ac38450b5de39812
RShnitser <romanshnitser@hotmail.com>
2024-10-30 20:19:01 -0400

    add pick starter state

--- repl.go
+++ repl.go
@@ -26,12 +26,45 @@ type gameState struct{
 	running bool
 	stamina int	
 	scene Scene
+	pokemon pokemon
 }
 
 func (state *gameState)init(){
 	state.running = true
 	state.stamina = 10
 	state.scene = Start
+}
+
+func getInput(scanner *bufio.Scanner)string{
+	scanner.Scan()
+	input := scanner.Text()
+	input = strings.ToLower(input)
+	return input
+}
+
+func printStart(){
+	fmt.Println("Pick your partner:")
+	fmt.Println("[1] Bulbasaur:")
+	fmt.Println("[2] Charmander")
+	fmt.Println("[3] Squirtle")
+}
+
+func processInputStart(state *gameState, input string){
+	
+	valid := true
+	switch(input){
+		case "1":
+			state.pokemon = pokemon{"Bulbasaur", nil}
+		case "2":
+			state.pokemon = pokemon{"Charmander", nil}
+		case "3":
+			state.pokemon = pokemon{"Squirtle", nil}
+		default:
+			valid = false
+	}
+	if valid{
+		state.scene = Travel
+	}
 }
 
 func (state *gameState)run(){
@@ -41,15 +74,11 @@ func (state *gameState)run(){
 
 		switch(state.scene){
 		case Start:
-			fmt.Println("Pick your partner:")
-			fmt.Println("[1] Bulbasaur:")
-			fmt.Println("[2] Charmander")
-			fmt.Println("[3] Squirtle")
+			printStart()
 		}
-		scanner.Scan()
-		input := scanner.Text()
-		input = strings.ToLower(input)
-
+		
+		input := getInput(scanner)
+		processInputStart(state, input)
 		if input == "exit"{
 			state.running = false
 		}
