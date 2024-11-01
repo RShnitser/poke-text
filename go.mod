b12442ceca317a6dd16706c9b79345c1aae648f0
RShnitser <romanshnitser@hotmail.com>
2024-10-31 20:36:31 -0400

    add env file

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module poke-text
 
 go 1.22.2
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sashabaranov/go-openai v1.32.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,46 @@
 package main
 
+import (
+	"os"
+	"fmt"
+	"context"
 
+	 "github.com/joho/godotenv"
+	 openai "github.com/sashabaranov/go-openai"
+)
 
 func main(){
 	//brightnessArray := "Ñ@#W$9876543210?!abc;:+=-,._ "
 	//fmt.Println(brightnessArray)
+
+	err := godotenv.Load(".env")
+	if err != nil{
+	 fmt.Printf("Error loading .env file: %s", err)
+	 os.Exit(1)
+	}
+   
+	api := os.Getenv("OPENAI_API_KEY")
+   
+	// client := openai.NewClient(api)
+	// resp, err := client.CreateChatCompletion(
+	// 	context.Background(),
+	// 	openai.ChatCompletionRequest{
+	// 		Model: openai.GPT3Dot5Turbo,
+	// 		Messages: []openai.ChatCompletionMessage{
+	// 			{
+	// 				Role:    openai.ChatMessageRoleUser,
+	// 				Content: "Hello!",
+	// 			},
+	// 		},
+	// 	},
+	// )
+
+	// if err != nil {
+	// 	fmt.Printf("ChatCompletion error: %v\n", err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println(resp.Choices[0].Message.Content)
 
 	state := gameState{}
 	state.init()
